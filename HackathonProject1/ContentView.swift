c6deeb69f78302db7fea1a6d8276e6af4c1cdd43
MiaKoring <143010553+MiaKoring@users.noreply.github.com>
2024-07-14 03:49:27 -0700

    Initial commit

---
+++ HackathonProject1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HackathonProject1
+//
+//  Created by Mia Koring on 14.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HackathonProject1/HackathonProject1App.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonProject1App.swift
+//  HackathonProject1
+//
+//  Created by Mia Koring on 14.07.24.
+//
+
+import SwiftUI
+
+@main
+struct HackathonProject1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
