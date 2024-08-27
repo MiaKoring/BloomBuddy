ceb300ef435f19792fc6fdc707acd11ee9ba6700
MiaKoring <143010553+MiaKoring@users.noreply.github.com>
2024-08-27 12:41:26 +0200

    Added Sensordata on Cardfront
    TODO: FAQ Sheet

--- BloomBuddy/App/Plants/PlantRowFront.swift
+++ BloomBuddy/App/Plants/PlantRowFront.swift
@@ -8,9 +8,11 @@
 import SwiftUI
 
 struct PlantRowFront: View {
+    @Environment(SensorManager.self) var sensorManager
 
     let cardColor: Color
     let plant: Plant?
+    @State var showHelp = false
 
     var body: some View {
         VStack {
@@ -22,6 +24,20 @@ struct PlantRowFront: View {
                 .frame(maxWidth: .infinity)
 
             PlantImage(80, "plantBg", color: .constant(cardColor), data: .constant(plant?.image), showButtons: .constant(false))
+                .overlay(alignment: .bottomTrailing) {
+                    if let plant, let sensorID = plant.sensor {
+                        if let sensor = sensorManager.sensordata?.first(where: {$0.id == sensorID}), let updated = sensor.updated?.double, Date.now.timeIntervalSince1970 - updated >= 10800 {
+                            Image(systemName: "exclamationmark.triangle.fill")
+                                .symbolRenderingMode(.palette)
+                                .foregroundStyle(.white, .orange)
+                                .font(.title)
+                                .offset(x: 15)
+                                .onTapGesture {
+                                    showHelp = true
+                                }
+                        }
+                    }
+                }
 
             HStack {
                 VStack {
@@ -30,7 +46,17 @@ struct PlantRowFront: View {
                         .foregroundStyle(.blue.lighter())
                         .frame(width: 20, height: 15)
                     
-                    if let plant {
+                    if let plant, let sensorID = plant.sensor {
+                        if let sensorData = sensorManager.sensordata?.first(where: {$0.id == sensorID})?.latest {
+                            Text("\(sensorData.int)%")
+                                .foregroundStyle(.gray)
+                                .font(.Bold.small)
+                        } else {
+                            Text("-")
+                                .foregroundStyle(.gray)
+                                .font(.Bold.small)
+                        }
+                    } else if let plant {
                         Text(WaterRequirement(percent: plant.waterRequirement).title)
                             .foregroundStyle(.gray)
                             .font(.Bold.small)
@@ -42,14 +68,30 @@ struct PlantRowFront: View {
                     .frame(width: 1, height: 20.0)
 
                 VStack {
-                    Image(systemName: "ruler")
-                        .font(.Regular.regularSmall)
-                        .foregroundStyle(.orange)
-                        .frame(width: 20, height: 15)
-
-                    Text("\(plant?.size.roundedInt ?? 0) cm")
-                        .foregroundStyle(.gray)
-                        .font(.Bold.small)
+                    if let plant, let sensorID = plant.sensor {
+                        Image(systemName: "clock")
+                            .font(.Regular.regularSmall)
+                            .foregroundStyle(.orange)
+                            .frame(width: 20, height: 15)
+                        if let sensor = sensorManager.sensordata?.first(where: {$0.id == sensorID}), let updated = sensor.updated {
+                            Text(Date.hmAgo(date: Date(timeIntervalSinceReferenceDate: updated.double)))
+                                .foregroundStyle(.gray)
+                                .font(.Bold.small)
+                        } else {
+                            Text("-")
+                                .foregroundStyle(.gray)
+                                .font(.Bold.small)
+                        }
+                    } else {
+                        Image(systemName: "ruler")
+                            .font(.Regular.regularSmall)
+                            .foregroundStyle(.orange)
+                            .frame(width: 20, height: 15)
+                        
+                        Text("\(plant?.size.roundedInt ?? 0) cm")
+                            .foregroundStyle(.gray)
+                            .font(.Bold.small)
+                    }
                 }
                 .frame(maxWidth: .infinity)
             }
@@ -58,5 +100,8 @@ struct PlantRowFront: View {
         .padding(10)
         .background(cardColor.gradient.opacity(0.15))
         .clipShape(.rect(cornerRadius: 15.0))
+        .sheet(isPresented: $showHelp) {
+            Text("Hilfe")
+        }
     }
 }
---
+++ BloomBuddy/Common/Extensions/Date.swift
@@ -0,0 +1,27 @@
+//
+//  Date.swift
+//  BloomBuddy
+//
+//  Created by Mia Koring on 27.08.24.
+//
+import SwiftChameleon
+import Foundation
+
+extension Date {
+    static func hmAgo(date: Date) -> String {
+        let difference = Date.now.timeIntervalSinceReferenceDate - date.timeIntervalSinceReferenceDate
+        switch difference {
+        case 0...59:
+            return "\(difference)s"
+        case 60...3589:
+            let minutes = (difference / 60.0).roundedInt
+            return "\(minutes)m"
+        case 3590...79100:
+            let hours = (difference / 3600.0).roundedInt
+            return "\(hours)h"
+        default:
+            let days = (difference / 79200.0).roundedInt
+            return "\(days)d"
+        }
+    }
+}
